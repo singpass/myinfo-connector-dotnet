7841fe038fe10701536aff6d8b05a2e8b311b84e
Brent <37363601+brent-ir@users.noreply.github.com>
2022-01-13 21:56:18 +1100

    Update build/GlobalAssemblyInfo.cs
    Co-authored-by: Malcolm <50188720+177alcol177@users.noreply.github.com>

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Singapore Gov")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCompany("Government of Singapore")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
